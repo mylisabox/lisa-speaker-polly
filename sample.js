888ea620db65f87eb82fc79e3f740e12752d598b
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-08-25 18:40:20 +0200

    v1.0.0

--- sample.js
+++ sample.js
@@ -3,6 +3,6 @@
 const speaker = require('./index')
 
 speaker.init({ language: 'fr-FR' })
-    .then(() => {
-        return speaker.speak('Bonjour, je m\'appel lisa')
-    })
+  .then(() => {
+    return speaker.speak('Bonjour, je m\'appel lisa, je suis nouvelle par ici, pourrais tu m\'aider ? ')
+  })
